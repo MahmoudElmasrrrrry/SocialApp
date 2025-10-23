aef7c90db559a9845610da3313a8d973d7abc649
MahmoudElmasrrrrry <mahmoudelmasry853j@gmail.com>
2025-10-24 01:13:05 +0300

    v1

--- dist/utils/errors/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApplicationError = void 0;
-class ApplicationError extends Error {
-    constructor(msg, statusCode, options) {
-        super(msg, options);
-    }
-}
-exports.ApplicationError = ApplicationError;
